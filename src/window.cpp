82a28e439a8c4d9d05dae553352f25ad45afa640
wptStation <zackanit@gmail.com>
2025-06-17 17:00:45 +0200

    half through window

--- src/window.cpp
+++ src/window.cpp
@@ -1,2 +1,17 @@
-#include <SDL2/
-#include <
+#include <SDL2/SDL.h>
+#include <cstdlib>
+
+
+void print_random_data(SDL_Surface* surface)
+{
+    int bytesPerPixel = surface->format->BytesPerPixel;
+}
+
+int main()
+{
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window *window = SDL_CreateWindow("Blinking Screen", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 900, 700, 0);
+    SDL_Surface *surface = SDL_GetWindowSurface(window);
+    print_random_data(surface);
+    SDL_Delay(500);
+}
